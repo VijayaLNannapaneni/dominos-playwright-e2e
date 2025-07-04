13227e444ca41f5c447bedbf54b77a6b472dbca5
vijayanannapaneni <vijayanalamothu@gmail.com>
2025-07-04 12:01:45 -0500

    Added cucumber HTML report integration

---
+++ generate-report.ts
@@ -0,0 +1,25 @@
+import { generate } from 'multiple-cucumber-html-reporter';
+
+generate({
+  jsonDir: 'reports',
+  reportPath: 'reports/html-report',
+  metadata: {
+    browser: {
+      name: 'chrome',
+      version: '91',
+    },
+    device: 'Local machine',
+    platform: {
+      name: process.platform,
+      version: '',
+    },
+  },
+  customData: {
+    title: 'Project Info',
+    data: [
+      { label: 'Project', value: 'Dominos Playwright Tests' },
+      { label: 'Test Environment', value: 'Local' },
+      { label: 'Executed', value: new Date().toLocaleString() },
+    ],
+  },
+});
---
+++ multiple-cucumber-html-reporter.d.ts
@@ -0,0 +1,2 @@
+// multiple-cucumber-html-reporter.d.ts
+declare module 'multiple-cucumber-html-reporter';
